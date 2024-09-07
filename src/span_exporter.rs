0d4fbb3f6af2361d588c4001a340fa66eb075c30
Abdulla Abdurakhmanov <me@abdolence.dev>
2024-09-07 11:06:35 +0200

    Removed unused method

--- src/span_exporter.rs
+++ src/span_exporter.rs
@@ -1,9 +1,7 @@
-use crate::errors::*;
 use crate::google_trace_exporter_client::GcpCloudTraceExporterClient;
 use crate::TraceExportResult;
 use futures::future::{BoxFuture, TryFutureExt};
 use futures::FutureExt;
-use gcloud_sdk::GoogleEnvironment;
 use opentelemetry_sdk::{
     export::trace::{ExportResult, SpanData, SpanExporter},
     Resource,
@@ -22,17 +20,6 @@ impl GcpCloudTraceExporter {
                 GcpCloudTraceExporterClient::new(google_project_id, resource).await?,
             ),
         })
-    }
-
-    pub async fn for_default_project_id() -> TraceExportResult<Self> {
-        let detected_project_id = GoogleEnvironment::detect_google_project_id().await.ok_or_else(||
-            GcloudTraceError::SystemError(
-                GcloudTraceSystemError::new(
-                    "No Google Project ID detected. Please specify it explicitly using env variable: PROJECT_ID or define it as default project for your service accounts".to_string()
-                )
-            )
-        )?;
-        Self::new(detected_project_id.as_str(), Resource::default()).await
     }
 }
 
